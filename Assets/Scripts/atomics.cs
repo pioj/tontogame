56b72476a913dd9e8319307c6e4463a90b1ad57b
pioj <piojete@gmail.com>
2019-07-24 11:40:31 +0200

    Requisito y generics
    Interfaz generica para Requisitos, y 3 mini-clases de Requisito (unlock, tiempo y recurso).
    
    Clase comun Requisito que puede implementar cualquiera de las 3 miniclases...

--- Assets/Scripts/atomics.cs
+++ Assets/Scripts/atomics.cs
@@ -1,4 +1,6 @@
-﻿public enum E_Requisito {
+﻿#region Tipos de Cosas
+
+public enum E_Requisito {
     TIEMPO,
     RECURSO,
     UNLOCKED
@@ -16,39 +18,113 @@ public enum E_Unidad {
     FRIGATE,
     DESTRUCTOR
 }
+#endregion
 
-
-public interface I_Rateable {
+#region Interfaces varios
+public interface I_Rateable
+{
     void UpdateRate();
 }
 
+public interface IRequisitable<T>
+{
+    void Check();
+    float Progreso(T genericCompared);
+}
+#endregion
 
-public class Requisito {
-    public E_Requisito Tipo;
-    public bool cumplido;
-    public int? valor= null;
+#region requisitables miniclases
 
-    public Requisito(E_Requisito tip, int comp) {
-        if (tip == E_Requisito.RECURSO) {
-            Tipo = tip;
-            valor = comp;
-            cumplido = false;
-        }
-        else if (tip == E_Requisito.TIEMPO) {
-            Tipo = tip;
-            valor = comp;
-            cumplido = false;
-        }
-    }
+public class Req_Unlock : IRequisitable<bool> {
+    public string condicion;
 
-    public Requisito(E_Requisito tip) {
-        if (tip == E_Requisito.UNLOCKED) {
-            Tipo = tip;
-            cumplido = false;
-        }
+    public void Check() { }
+
+    public float Progreso(bool cosa) {
+        float temp = 0f;
+        if (cosa == true) temp = 1f;
+        return temp;
     }
 }
 
+public class Req_Recurso : IRequisitable<int> {
+    public int cantidad;
+
+    public Req_Recurso(int cant = 100) {
+        cantidad = cant;
+    }
+
+    public void Check() { }
+
+    public float Progreso(int cant) {
+        float temp = 0f;
+        temp = ((float)cant / (float)cantidad) * 100f;
+        return temp;
+    }
+}
+
+public class Req_Tiempo : IRequisitable<double> {
+    public double totalsecs;
+    public string timestring;
+
+    public Req_Tiempo(string cadena) {
+        timestring = cadena;
+        totalsecs = GetTotalSecs(cadena);
+    }
+
+    public Req_Tiempo(double segs) {
+        totalsecs = segs;
+        timestring = System.TimeSpan.FromSeconds(segs).ToString("yyyyMMddHHmmss");
+    }
+
+    public double GetTotalSecs(string cadena) {
+        double temp = 0;
+        if (cadena != null && cadena.Length > 0) temp = System.Convert.ToDouble(cadena);
+        return temp;
+    }
+
+    public float Progreso(double currentsecs) {
+        float temp =  0f;
+        temp = (float)(currentsecs / totalsecs) * 100f;
+        //return System.Math.Round(temp, 2);
+        return temp;
+    }
+    
+    public void Check() { }
+}
+#endregion 
+
+
+#region Requisitos
+
+public class Requisito<T>
+{
+    public T obj;
+    public E_Requisito Tipo;
+    public bool cumplido;
+    public float progreso;
+
+    public Requisito(T miclase, E_Requisito tip) {
+        this.obj = miclase;
+        cumplido = false;
+        Tipo = tip;
+        progreso = 0f;
+    }
+
+    public Requisito(T miclase) {
+        this.obj = miclase;
+        cumplido = false;
+        progreso = 0f;
+        if (miclase.GetType() == typeof(Req_Tiempo)) Tipo = E_Requisito.TIEMPO;
+        if (miclase.GetType() == typeof(Req_Recurso)) Tipo = E_Requisito.RECURSO;
+        if (miclase.GetType() == typeof(Req_Unlock)) Tipo = E_Requisito.UNLOCKED;
+    }
+}
+
+#endregion
+
+
+#region Unidades del Hangar
 public class Unidad {
     public E_Unidad Tipo;
     public uint Cantidad;
@@ -58,7 +134,9 @@ public class Unidad {
         Cantidad = 0;
     }
 }
+#endregion
 
+#region Edificios y Construcciones
 
 public class ConstructParent {
     public E_Construccion Categoria;
@@ -82,3 +160,4 @@ public class ConstructParent {
     public void RefreshData() { }
 }
 
+#endregion
